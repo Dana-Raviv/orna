eee8bc2fb145b2651e106fe02487c6386fccd05e
Dana-Raviv <75971492+Dana-Raviv@users.noreply.github.com>
2022-09-11 11:15:32 +0300

    loader

--- index.hyperesources/index_hype_generated_script.js
+++ index.hyperesources/index_hype_generated_script.js
@@ -4,4 +4,28 @@
 !1)try{for(var c=document.getElementsByTagName("script"),a=0;a<c.length;a++){var d=c[a].src,b=null!=d?d.indexOf("/index_hype_generated_script.js"):-1;if(-1!=b){l=d.substr(0,b);break}}}catch(p){}c=null==navigator.userAgentData&&navigator.userAgent.match(/MSIE (\d+\.\d+)/);c=parseFloat(c&&c[1])||null;d=!0==(null!=window.HYPE_740F||null!=window.HYPE_dtl_740F)||false==!0||null!=c&&10>c;a=!0==d?"HYPE-740.full.min.js":"HYPE-740.thin.min.js";c=!0==d?"F":"T";
 d=d?"":"";if(false==!1&&(a=k("HYPE_740"+c,"HYPE_dtl_740"+c,a,d),false==!0&&(a=a||k("HYPE_w_740","HYPE_wdtl_740","HYPE-740.waypoints.min.js","")),false==!0&&(a=a||k("Matter","HYPE_pdtl_740","HYPE-740.physics.min.js","")),a))return;d=window.HYPE.documents;if(null!=d[f]){b=1;a=f;do f=""+a+"-"+b++;while(null!=d[f]);for(var e=
 document.getElementsByTagName("div"),b=!1,a=0;a<e.length;a++)if(e[a].id==g&&null==e[a].getAttribute("HYP_dn")){var b=1,h=g;do g=""+h+"-"+b++;while(null!=document.getElementById(g));e[a].id=g;b=!0;break}if(!1==b)return}b=[];b=[];e={};h={};for(a=0;a<b.length;a++)try{h[b[a].identifier]=b[a].name,e[b[a].name]=eval("(function(){return "+b[a].source+"})();")}catch(n){window.console&&window.console.log(n),e[b[a].name]=function(){}}c=new window["HYPE_740"+c](f,g,{"-1":{n:"PIE.htc"},"-2":{n:"blank.gif"}},
-l,[],e,[{n:"Greetings from around the world Orna's 70th Birthday Extravaganza ",o:"1",X:[0]}],[{o:"3",p:"600px",a:100,Y:608,Z:402,b:100,cA:false,c:"#FFF",L:[],bY:1,d:608,U:{},T:{kTimelineDefaultIdentifier:{q:false,z:0,i:"kTimelineDefaultIdentifier",n:"Main Timeline",a:[],f:30,b:[]}},bZ:180,O:["10"],n:"Untitled Layout","_":0,v:{"10":{I:"None",x:"visible",J:"None",a:0,b:0,K:"None",bS:63,L:"None",j:"absolute",z:1,d:402,U:"iframe-htmlwidget.html",k:"div",c:608,V:0,W:""}}}],{},h,{},null,false,true,-1,true,true,false,true,true);d[f]=c.API;document.getElementById(g).setAttribute("HYP_dn",f);c.z_o(this.body)})();})();
+l,[],e,[{n:"Greetings from around the world Orna's 70th Birthday Extravaganza ",o:"1",X:[0]}],[{o:"3",p:"600px",a:100,Y:608,Z:402,b:100,cA:false,c:"#000",L:[],bY:1,d:608,U:{},T:{kTimelineDefaultIdentifier:{q:false,z:0,i:"kTimelineDefaultIdentifier",n:"Main Timeline",a:[],f:30,b:[]}},bZ:180,O:["10"],n:"Untitled Layout","_":0,v:{"10":{I:"None",x:"visible",J:"None",a:0,b:0,K:"None",bS:63,L:"None",j:"absolute",z:1,d:402,U:"iframe-htmlwidget.html",k:"div",c:608,V:0,W:""}}}],{},h,{},
+(function (shouldShow, mainContentContainer) {
+	var loadingPageID = mainContentContainer.id + "_loading";
+	var loadingDiv = document.getElementById(loadingPageID);
+
+	if(shouldShow == true) {
+		if(loadingDiv == null) {	
+			loadingDiv = document.createElement("div");
+			loadingDiv.id = loadingPageID;
+			loadingDiv.style.cssText = "overflow:hidden;position:absolute;width:150px;top:40%;left:0;right:0;margin:auto;padding:2px;border:3px solid #BBB;background-color:#EEE;border-radius:10px;text-align:center;font-family:Helvetica,Sans-Serif;font-size:13px;font-weight:700;color:#AAA;z-index:100000;";
+			loadingDiv.innerHTML = "Loading";
+			mainContentContainer.appendChild(loadingDiv);
+		}
+ 
+		loadingDiv.style.display = "block";
+		loadingDiv.removeAttribute("aria-hidden");
+		mainContentContainer.setAttribute("aria-busy", true);
+	} else {
+		loadingDiv.style.display = "none";
+		loadingDiv.setAttribute("aria-hidden", true);
+		mainContentContainer.removeAttribute("aria-busy");
+	}
+})
+
+,false,true,-1,true,true,false,true,true);d[f]=c.API;document.getElementById(g).setAttribute("HYP_dn",f);c.z_o(this.body)})();})();
